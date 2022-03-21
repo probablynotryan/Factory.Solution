67b20877e64c2176cc3e9fc165bc1b7292280650
Ryan Bass <theryanbass@gmail.com>
2022-03-20 22:48:47 -0700

    add e/m lists on home index

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Factory.Models;
 
 namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly FactoryContext _db;
+      public HomeController(FactoryContext db)
+      {
+        _db = db;
+      }
+
 
       [HttpGet("/")]
       public ActionResult Index()
       {
+        ViewBag.E = _db.Engineers.ToList();
+        ViewBag.M = _db.Machines.ToList();
         return View();
       }
 
